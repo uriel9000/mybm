75d1c273b92e7a4d83702283b92e8676d76ea630
uriel9000 <136067238+uriel9000@users.noreply.github.com>
2025-06-16 10:52:07 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+// service-worker.js
+const CACHE_NAME = 'bmi-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  'https://cdn.tailwindcss.com',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
